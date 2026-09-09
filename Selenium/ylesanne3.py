210123d893d4d9088472cdf7f41a7c17a2ed0bb1
M1tras <108801657+M1tras@users.noreply.github.com>
2026-09-09 09:27:08 +0300

    muudatused

--- Selenium/ylesanne3.py
+++ Selenium/ylesanne3.py
@@ -10,9 +10,20 @@ driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
 time.sleep(2)
 
 add_button = driver.find_element(By.TAG_NAME, "button")
+add_button = driver.find_element(By.XPATH, "//button[text()='Add Element']")
+
+for _ in range(5):
+    add_button.click()
+    time.sleep(0.5)
+
+delete_buttons = driver.find_elements(By.CLASS_NAME, "added-manually")
 
 add_button.click()
+for button in delete_buttons:
+    button.click()
+    time.sleep(0.5)
 
 input()
+input("Vajuta Enter brauseri sulgemiseks...")
 
 driver.quit()
